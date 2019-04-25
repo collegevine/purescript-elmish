ef1c6722c3734bc209cdef2b638d1ea29445d807
Fyodor Soikin <name.fa@gmail.com>
2019-04-24 20:16:59 -0400

    Remove non-FFI JS module

--- src/Elmish/Component.js
+++ src/Elmish/Component.js
@@ -1,17 +1,29 @@
 const React = require("react")
-const component = require("Elmish/ComponentClass")
 
 exports.withCachedComponent = (function() {
   const cache = {}
 
   return function(name, f) {
-    const c = cache[name] || (cache[name] = component.mkFreshComponent())
+    const c = cache[name] || (cache[name] = mkFreshComponent())
     return f(c)
   }
 })()
 
 exports.withFreshComponent = function(f) {
-  return f(component.mkFreshComponent())
+  return f(mkFreshComponent())
 }
 
 exports.instantiateBaseComponent = React.createElement
+
+export const mkFreshComponent = function() {
+  function ElmishComponent() {}
+  ElmishComponent.prototype = Object.create(React.Component.prototype)
+  ElmishComponent.prototype.render = function() {
+    return this.props.render(this)()
+  }
+  ElmishComponent.prototype.componentDidMount = function() {
+    this.props.componentDidMount(this)()
+  }
+
+  return ElmishComponent
+}
