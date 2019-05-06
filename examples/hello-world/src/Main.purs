3d2030001b3828074b0184d263e94e303c4879f7
Daniel Gasienica <daniel@gasienica.ch>
2019-05-05 23:16:00 -0400

    Render Elmish/React Hello World

--- examples/hello-world/src/Main.purs
+++ examples/hello-world/src/Main.purs
@@ -2,8 +2,33 @@ module Main where
 
 import Prelude
 
+import Data.Function.Uncurried (runFn2)
+import Data.Maybe (Maybe(..))
 import Effect (Effect)
-import Effect.Console (log)
+import Effect.Console as Console
+import Web.DOM (Element)
+import Web.DOM.Document (toNonElementParentNode) as DOM
+import Web.DOM.NonElementParentNode (getElementById) as DOM
+import Web.HTML (window) as DOM
+import Web.HTML.HTMLDocument (toDocument) as DOM
+import Web.HTML.Window (document) as DOM
+
+import Elmish.React as R
+import Elmish.React.DOM as R
+
+
+getElementById :: String -> Effect (Maybe Element)
+getElementById id =
+    DOM.getElementById id
+    =<< (pure <<< DOM.toNonElementParentNode <<< DOM.toDocument)
+    =<< DOM.document
+    =<< DOM.window
 
 main :: Effect Unit
-main = log "Hello World"
+main = do
+    mApp <- getElementById "app"
+    case mApp of
+        Just app ->
+            runFn2 R.reactMount app (R.text "Hello World")
+        Nothing ->
+            Console.error "Couldn’t find #app container"
