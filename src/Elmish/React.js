7b3e9e72c9ff8870d423aa0e29d5a9fd9e90962a
Daniel Gasienica <daniel@gasienica.ch>
2020-02-14 12:51:36 +0100

    Document `data-` HTML attributes API

--- src/Elmish/React.js
+++ src/Elmish/React.js
@@ -36,12 +36,28 @@ exports.createElement_ = function(component, props, children) {
   //
   //  return React.createElement(component, props, ...children)
   //
+  //  HTML `data-` attributes
+  //  =======================
+  //
+  // `props` accepts a special-cased `_data` object that is translated into HTML
+  // `data-` attributes, e.g.
+  //
+  //  var component = "div"
+  //  var props = { id: "viewer", _data: { toggle: "buttons", label: "toolbar" } }
+  //
+  //  results in
+  //
+  //  <div id="viewer" data-toggle="buttons" data-label="toolbar" />
+  //
   return React.createElement.apply(
     null,
     [component, flattenDataProp(component, props)].concat(children)
   )
 }
 
+// Flattens special `_data` `props` object into `data-` HTML attributes. This
+// is skipped for non-HTML components, i.e. custom components, as well as if
+// `_data` is not present in `props`:
 function flattenDataProp(component, props) {
   if (typeof component !== "string" || props._data == null) {
     return props
