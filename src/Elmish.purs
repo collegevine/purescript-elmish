c696e15b4d886eb73276ed1ade6aa5049e41122e
Fyodor Soikin <name.fa@gmail.com>
2021-03-14 21:08:08 -0400

    Operators

--- src/Elmish.purs
+++ src/Elmish.purs
@@ -8,6 +8,6 @@ module Elmish
 
 import Elmish.Boot (BootRecord, boot)
 import Elmish.Component (ComponentDef, Transition(..), bimap, construct, fork, forks, forkVoid, forkMaybe, lmap, nat, rmap, transition, withTrace)
-import Elmish.Dispatch (Dispatch, EffectFn1, EffectFn2, handle, handleMaybe, mkEffectFn1, mkEffectFn2)
+import Elmish.Dispatch (Dispatch, EffectFn1, EffectFn2, handle, handleMaybe, mkEffectFn1, mkEffectFn2, (<|), (<?|))
 import Elmish.JsCallback (JsCallback, JsCallback0, jsCallback)
 import Elmish.React (ReactComponent, ReactElement, createElement, createElement')
--- src/Elmish/Dispatch.purs
+++ src/Elmish/Dispatch.purs
@@ -2,6 +2,8 @@ module Elmish.Dispatch
     ( Dispatch
     , handle
     , handleMaybe
+    , (<|)
+    , (<?|)
     , module E
     ) where
 
@@ -17,8 +19,27 @@ type Dispatch msg = msg -> Effect Unit
 infixr 9 handle as <|
 infixr 9 handleMaybe as <?|
 
+-- | A convenience function to make construction of event handlers with
+-- | arguments (i.e. `EffectFn1`) a bit shorter. The function takes a `Dispatch`
+-- | and a mapping from the event argument to a message which the given
+-- | `Dispatch` accepts, and it's also available in operator form.
+-- |
+-- | The following example demonstrates expected usage of both `handle` (in its
+-- | operator form `<|`) and `handleMaybe` (in its operator form `<?|`):
+-- |
+-- |     textarea
+-- |       { value: state.text
+-- |       , onChange: dispatch <?| \e -> TextChanged <$> eventTargetValue e
+-- |       , onMouseDown: dispatch <| \_ -> TextareaClicked
+-- |       }
+-- |
+-- |       where
+-- |         eventTargetValue = readForeign >=> lookup "target" >=> readForeign >=> lookup "value"
+-- |
 handle :: forall arg msg. Dispatch msg -> (arg -> msg) -> E.EffectFn1 arg Unit
 handle dispatch fn = E.mkEffectFn1 $ dispatch <<< fn
 
+-- | Same as `handle`, but dispatches a message optionally. See comments on
+-- | `handle` for an example.
 handleMaybe :: forall arg msg. Dispatch msg -> (arg -> Maybe msg) -> E.EffectFn1 arg Unit
 handleMaybe dispatch fn = E.mkEffectFn1 $ maybe (pure unit) dispatch <<< fn
