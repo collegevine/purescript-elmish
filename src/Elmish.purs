97fb6cde2a438c98335ee17331f79452aaf37b58
Marc Cordova <marc.cordova@collegevine.com>
2021-11-03 22:27:33 -0400

    Use Aff by default (#48)

--- src/Elmish.purs
+++ src/Elmish.purs
@@ -7,7 +7,7 @@ module Elmish
     ) where
 
 import Elmish.Boot (BootRecord, boot)
-import Elmish.Component (ComponentDef, Transition(..), bimap, construct, fork, forks, forkVoid, forkMaybe, lmap, nat, rmap, transition, withTrace)
+import Elmish.Component (ComponentDef, ComponentDef', Transition, Transition'(..), bimap, construct, fork, forks, forkVoid, forkMaybe, lmap, nat, rmap, transition, withTrace)
 import Elmish.Dispatch (Dispatch, EffectFn1, EffectFn2, handle, handleMaybe, mkEffectFn1, mkEffectFn2, (<|), (<?|))
 import Elmish.JsCallback (JsCallback, JsCallback0, jsCallback)
 import Elmish.React (ReactComponent, ReactElement, createElement, createElement')
