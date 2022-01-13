fa654af82fd914b2541ee1fa64a2f4c2c38c0b61
Fyodor Soikin <name.fa@gmail.com>
2022-01-13 14:54:29 -0500

    Remove JsCallback (#53)

--- src/Elmish.purs
+++ src/Elmish.purs
@@ -2,12 +2,10 @@ module Elmish
     ( module Elmish.Boot
     , module Elmish.Component
     , module Elmish.Dispatch
-    , module Elmish.JsCallback
     , module Elmish.React
     ) where
 
 import Elmish.Boot (BootRecord, boot)
 import Elmish.Component (ComponentDef, ComponentDef', Transition, Transition'(..), bimap, construct, fork, forks, forkVoid, forkMaybe, lmap, nat, rmap, transition, withTrace)
 import Elmish.Dispatch (Dispatch, EffectFn1, EffectFn2, handle, handleMaybe, mkEffectFn1, mkEffectFn2, (<|), (<?|))
-import Elmish.JsCallback (JsCallback, JsCallback0, jsCallback)
 import Elmish.React (ReactComponent, ReactElement, createElement, createElement')
