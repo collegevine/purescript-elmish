cef8bf1a0a899d8ff130e1c1644045b55e397425
Marc Cordova <mcordova47@gmail.com>
2022-04-18 23:05:52 -0400

    Consolidate some cases

--- src/Elmish/React/Ref.purs
+++ src/Elmish/React/Ref.purs
@@ -25,11 +25,10 @@ instance CanPassToJavaScript (Ref a)
 callbackRef :: forall el. Maybe el -> (Maybe el -> Effect Unit) -> Ref el
 callbackRef ref setRef = mkCallbackRef $ setRef <?| \ref' -> case ref, Nullable.toMaybe ref' of
   Nothing, Nothing -> Nothing
-  _, Nothing -> Just Nothing
-  Nothing, r -> Just r
   Just r, Just r'
     | eqByReference r r' -> Nothing
     | otherwise -> Just $ Just r'
+  _, r -> Just r
   where
     mkCallbackRef :: EffectFn1 (Nullable el) Unit -> Ref el
     mkCallbackRef = unsafeCoerce
