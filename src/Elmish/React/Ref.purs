ec0a3dc6b668210316eeff28cfdb01101d4f41d5
Marc Cordova <mcordova47@gmail.com>
2022-04-18 22:34:32 -0400

    Document unsafeCoerce

--- src/Elmish/React/Ref.purs
+++ src/Elmish/React/Ref.purs
@@ -8,6 +8,7 @@ import Prelude
 
 import Data.Maybe (Maybe(..))
 import Effect (Effect)
+import Effect.Uncurried (EffectFn1)
 import Elmish.Dispatch ((<?|))
 import Elmish.Foreign (class CanPassToJavaScript)
 import Unsafe.Coerce (unsafeCoerce)
@@ -17,8 +18,11 @@ data Ref (el :: Type)
 instance CanPassToJavaScript (Ref a)
 
 callbackRef :: forall el. Maybe el -> (el -> Effect Unit) -> Ref el
-callbackRef ref setRef = unsafeCoerce $ setRef <?| \r -> case eqByReference r <$> ref of
+callbackRef ref setRef = mkCallbackRef $ setRef <?| \r -> case eqByReference r <$> ref of
   Just true -> Nothing
   _ -> Just r
+  where
+    mkCallbackRef :: EffectFn1 el Unit -> Ref el
+    mkCallbackRef = unsafeCoerce
 
 foreign import eqByReference :: forall a. a -> a -> Boolean
