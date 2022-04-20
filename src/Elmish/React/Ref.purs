95c5667905feaf2760dc23fbb16a8787c51410ae
Marc Cordova <mcordova47@gmail.com>
2022-04-20 10:48:46 -0400

    Update docs

--- src/Elmish/React/Ref.purs
+++ src/Elmish/React/Ref.purs
@@ -20,8 +20,19 @@ data Ref (el :: Type)
 
 instance CanPassToJavaScript (Ref a)
 
--- | Turns a callback function (`el -> Effect Unit`) into a `Ref`. The callback
--- | function should add the `el` parameter to some state.
+-- | Takes the current ref value and a callback function (`el -> Effect Unit`)
+-- | and returns a `Ref`. The current ref value is needed so that we can decide
+-- | whether the callback function should be run (by comparing the current ref
+-- | and the new one by reference). The callback function should add the `el`
+-- | parameter to some state. E.g.:
+-- |
+-- | ```purs
+-- | data Message = RefChanged (Maybe HTMLInputElement) | …
+-- |
+-- | view :: State -> Dispatch Message -> ReactElement
+-- | view state dispatch =
+-- |   H.input_ "" { ref: callbackRef state.inputElement (dispatch <<< RefChanged), … }
+-- | ```
 callbackRef :: forall el. Maybe el -> (Maybe el -> Effect Unit) -> Ref el
 callbackRef ref setRef = mkCallbackRef $ setRef <?| \ref' -> case ref, Nullable.toMaybe ref' of
   Nothing, Nothing -> Nothing
